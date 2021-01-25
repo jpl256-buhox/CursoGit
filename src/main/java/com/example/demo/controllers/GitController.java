b84682d8c5422394adcfa9e0d3e1ba02f4a54dcf
Jesus Antonio Perez de Leon <jesus.perezd@cfe.mx>
2021-01-25 10:59:58 -0600

    Habilitacion enpoint Saludo

--- src/main/java/com/example/demo/controllers/GitController.java
+++ src/main/java/com/example/demo/controllers/GitController.java
@@ -1,9 +1,18 @@
 package com.example.demo.controllers;
 
 
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
+
+
+    @GetMapping(value="/saludo")
+    public void Saludo(@RequestParam String Nombre){
+        System.out.println("Hola mundo " + Nombre);
+    }
 
 }
