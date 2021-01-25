73d019183cf6e32e72fed6ea6dd5903a5e3ab6c9
Jesus Antonio Perez de Leon <jesus.perezd@cfe.mx>
2021-01-25 11:15:45 -0600

    Habilitacion de Logger

--- src/main/java/com/example/demo/controllers/GitController.java
+++ src/main/java/com/example/demo/controllers/GitController.java
@@ -2,17 +2,18 @@ package com.example.demo.controllers;
 
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitController {
-
+    Logger logger = LoggerFactory.getLogger(GitController.class);
 
     @GetMapping(value="/saludo")
     public void Saludo(@RequestParam String Nombre){
-        System.out.println("Hola mundo " + Nombre);
+        logger.info("Hola mundo " + Nombre);
     }
 
 }
